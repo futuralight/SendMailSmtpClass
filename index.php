14465eda417b9b5c70146eceb8f31244e1bae0c5
futuralight <futuralight@outlook.com>
2020-07-08 14:52:23 +0500

    XOAUTH

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	require_once "SendMailSmtpClass.php"; // подключаем класс
 	  
 	// примеры подключения	
-	$mailSMTP = new SendMailSmtpClass('zhenikipatov@yandex.ru', '***', 'ssl://smtp.yandex.ru', 465, "UTF-8");
+	$mailSMTP = new SendMailSmtpClass('m.yurin@ankas.ru', 'AgAEA7qjSAUEAAZdi2-ySeSWtEoJv-1t76nWiOY', 'ssl://smtp.yandex.ru', 465, "UTF-8");
 	// $mailSMTP = new SendMailSmtpClass('zhenikipatov@yandex.ru', '***', 'ssl://smtp.yandex.ru', 465, "windows-1251");
 	// $mailSMTP = new SendMailSmtpClass('monitor.test@mail.ru', '***', 'ssl://smtp.mail.ru', 465, "UTF-8");
 	// $mailSMTP = new SendMailSmtpClass('red@mega-dev.ru', '***', 'ssl://smtp.beget.com', 465, "UTF-8");
@@ -16,25 +16,35 @@
 	// от кого
 	$from = array(
 		"Евгений", // Имя отправителя
-		"test@vk-book.ru" // почта отправителя
+		"m.yurin@ankas.ru" // почта отправителя
 	);
 	// кому отправка. Можно указывать несколько получателей через запятую
-	$to = 'admin@vk-book.ru, ipatovsoft@gmail.com';
+	$to = 'greedthegangboss@gmail.com';
 	
 	// добавляем файлы
 	$mailSMTP->addFile("test.jpg");
 	$mailSMTP->addFile("test2.jpg");
 	$mailSMTP->addFile("test3.txt");
-	
+
+	// $mailSMTP->addFileBase64();
 	// добавить получателя письма в копию
-	$mailSMTP->toCopy("test-copy@yandex.ru"); 
-	$mailSMTP->toCopy("test-copy@vk-book.ru");
+	// $mailSMTP->toCopy("test-copy@yandex.ru"); 
+	// $mailSMTP->toCopy("test-copy@vk-book.ru");
 	
 	// добавить получателя письма в скрытую копию
-	$mailSMTP->toHideCopy("zhenikipatov@yandex.ru");
-	
+	// $mailSMTP->toHideCopy("zhenikipatov@yandex.ru");
+	$message = '<body style="margin: 0; padding: 0;">
+	<table border="1" cellpadding="0" cellspacing="0" width="100%">
+	 <tr>
+	  <td>
+	   Hello!
+	  </td>
+	 </tr>
+	</table>
+   </body>
+	';
 	// отправляем письмо
-	$result =  $mailSMTP->send($to, 'Тема письма с копиями ', 'Текст письма', $from); 
+	$result =  $mailSMTP->send($to, 'Тема письма с копиями ', $message, $from); 
 	// $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Отправитель письма');
 	
 	if($result === true){
